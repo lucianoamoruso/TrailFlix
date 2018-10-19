1fd3ab7cfa3e4d20c03537175784548d73d0d1bf
Luciano <luchisonico@gmail.com>
2018-10-18 21:27:19 -0300

    Estilo nav categorias
    • Cambio el renderizado de las categorias.
    • Creo un archivo css para modificar el ancho.

--- Flix-web/src/components/principal/Categorias.jsx
+++ Flix-web/src/components/principal/Categorias.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 
+import '../../dist/css/NavCategorias.css';
+
 export default class Categorias extends React.Component {
   render() {
     return (
-      <ul>
-        {this.props.categorias.map(cat => (
-          <li key={cat}>{cat}</li>
-        ))}
-      </ul>
+      <div>
+        <ul>
+          {this.props.categorias.map(cat => (
+            <li key={cat}>{cat}</li>
+          ))}
+        </ul>
+        <nav className="nav justify-content-end">
+          <button className="btn btn-outline-secondary" type="button">Drama</button>
+          <button className="btn btn-outline-secondary" type="button">Accion</button>
+          <button className="btn btn-outline-secondary" type="button">Comedia</button>
+          <button className="btn btn-outline-secondary" type="button">Terror</button>
+        </nav>
+      </div>
     );
   }
 }
+
+/*
+----- ALTERNATIVA -----
+<ul className="nav nav-pills nav-fill" role="tablist">
+<li className="nav-item">
+  <a className="nav-link" data-toggle="tab" href="#" role="tab" aria-selected="false">Active</a>
+</li>
+<li className="nav-item">
+<a className="nav-link" data-toggle="tab" href="#" role="tab" aria-selected="false">Longer nav link</a>
+</li>
+<li className="nav-item">
+<a className="nav-link" data-toggle="tab" href="#" role="tab" aria-selected="false">Link</a>
+</li>
+<li className="nav-item">
+<a className="nav-link" data-toggle="tab" href="#" role="tab" aria-selected="false">Disabled</a>
+</li>
+</ul>
+*/
