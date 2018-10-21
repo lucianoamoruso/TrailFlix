e235a504c25fe307b86b6dbb1473b0c205a493b2
Luciano <luchisonico@gmail.com>
2018-10-21 00:43:25 -0300

    CSS dinamico de Filtro
    Termino de crear el metodo para los border del header del filtro y elimino el antiguo filtro.

--- Flix-web/src/components/principal/Filtro.jsx
+++ Flix-web/src/components/principal/Filtro.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
 
 import '../../dist/css/Filtro.css';
+// import headerAbierto from './Filtro.ts';
 
 export default class Filtro extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { colapsado: true };
+    this.mostrarFiltros = this.mostrarFiltros.bind(this);
+  }
+
+  mostrarFiltros(e) {
+    if (this.state.colapsado === true) {
+      e.target.style.borderBottomLeftRadius = '0px';
+      e.target.style.borderBottomRightRadius = '0px';
+      this.setState({ colapsado: false });
+    } else {
+      e.target.style.borderBottomLeftRadius = '8px';
+      e.target.style.borderBottomRightRadius = '8px';
+      this.setState({ colapsado: true });
+    }
+  }
+
   render() {
     return (
-      <div>
-        <nav className="flex-column filter-nav">
-          <button className="btn btn-primary nav-item" type="button" data-toggle="collapse" data-target="#filtros" aria-expanded="false" aria-controls="collapseExample">
-            Filtrar:
-          </button>
-          <div className="collapse" id="filtros">
-            <button className="btn btn-outline-secondary nav-item" type="button"> Recomendados </button><br />
-            <button className="btn btn-outline-secondary nav-item" type="button"> Favoritos </button>
-          </div>
-        </nav>
-        //Segunda opcion
-        <nav className="flex-column filter-nav">
-          <a className="nav-link active" id="filter-header" data-toggle="collapse" data-target="#collapseExample" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Filtrar:</a>
-          <div className="collapse" id="collapseExample">
-            <a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Recomendados</a>
-            <a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Favoritos</a>
-          </div>
-        </nav>
-      </div>
-
+      <nav className="flex-column filter-nav">
+        <a className="nav-link active" id="filter-header" onClick={this.mostrarFiltros} data-toggle="collapse" data-target="#collapseExample" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Filtrar:</a>
+        <div className="collapse" id="collapseExample">
+          <a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Recomendados</a>
+          <a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Favoritos</a>
+        </div>
+      </nav>
     );
   }
 }
