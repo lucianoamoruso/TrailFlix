fe09e4d55ba191b6d9587fb2541a6c2a8193d9d3
Luciano <luchisonico@gmail.com>
2018-10-25 15:27:01 -0300

    Busqueda
    Creo el componente Tabla y la barra de busqueda.

--- Flix-web/src/components/principal/Home.jsx
+++ Flix-web/src/components/principal/Home.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 
 import Categorias from './Categorias';
 import Filtro from './Filtro';
+import Tabla from './Tabla';
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -16,15 +17,16 @@ export default class Home extends React.Component {
     this.handleRellenar = this.handleRellenar.bind(this);
   }
 
-  /*
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 500);
-  }
-  */
-
   handleRellenar() {
     this.setState({ categorias: ['Drama', 'Terror', 'Accion', 'Comedia'] });
-    console.log(this.state);
+  }
+
+  cambiarBusqueda(evento) {
+    this.setState({ buscar: evento.target.value });
+  }
+
+  filtrar() {
+    console.log();
   }
 
   render() {
@@ -33,6 +35,11 @@ export default class Home extends React.Component {
         <div className="row row-show">
           <div id="home-h1" className="col-2 offset-5 col-show">
             <h1>TrailFlix</h1>
+          </div>
+        </div>
+        <div className="row row-show">
+          <div className="col-2 offset-5 col-show">
+            <input id="busqueda" type="text" className="form-control" placeholder="Buscar..." onChange={e => this.cambiarBusqueda(e)} />
           </div>
         </div>
         <button type="button" onClick={this.handleRellenar}>Rellenar categorias</button>
@@ -44,6 +51,7 @@ export default class Home extends React.Component {
             <Categorias categorias={this.state.categorias} />
           </div>
         </div>
+        <Tabla />
       </div>
     );
   }
---
+++ Flix-web/src/components/principal/Tabla.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Tabla extends React.Component {
+  render() {
+    return(
+      
+    );
+  }
+}
