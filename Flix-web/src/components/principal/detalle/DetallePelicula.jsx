2fe0dd80270223ca7ea228f9d902f06193adbb9d
Luciano <luchisonico@gmail.com>
2018-11-06 14:58:49 -0300

    Estrella de Favorito
    • En index.html agrego Material Design.
    • En DetallePelicula ahora manejo los favoritos del Usuario: puedo agregar a o quitar de favoritos la pelicula y la estrella se rellena si la pelicula ya esta en favoritos, esto funciona incluso recargando la pagina. Tambien acomodo un poco el layout y agrego un boton de probar cosas.

--- Flix-web/src/components/principal/detalle/DetallePelicula.jsx
+++ Flix-web/src/components/principal/detalle/DetallePelicula.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable spaced-comment */
 import React from 'react';
 
 import API from '../../../service/api';
+
+import '../../../dist/css/principal/detalle/DetallePelicula.css';
 
 export default class DetallePelicula extends React.Component {
   constructor(props) {
@@ -23,14 +26,28 @@ export default class DetallePelicula extends React.Component {
         watched: false,
         recommendations: [],
       },
+      favoritos: [],
     };
     this.username = this.props.location.state !== undefined ? this.props.location.state.username : undefined;
-    this.codigo = this.props.match.params.id;
+    this.codigo = parseInt(this.props.match.params.id, 10);
+    this.fav = React.createRef();
   }
 
   componentDidMount() {
+    this.traerDatos();
+    this.traerFavs();
+    this.agregarListeners();
+  }
+
+  traerDatos() {
     API.get(`/${this.username}/movie/${this.codigo}`)
       .then(response => this.cargarDatos(response))
+      .catch();
+  }
+
+  traerFavs() {
+    API.get(`/${this.username}/favs`)
+      .then(response => this.cargarFavs(response))
       .catch();
   }
 
@@ -38,16 +55,49 @@ export default class DetallePelicula extends React.Component {
     this.setState({ pelicula: datos.contenido, info: datos.info });
   }
 
+  cargarFavs(datos) {
+    const nuevos = datos.map(cont => cont.codigo);
+    this.setState({ favoritos: nuevos });
+    this.fav.current.innerHTML = nuevos.includes(this.codigo) ? 'star' : 'star_border'; //Aca se actualiza el icono de fav
+  }
+
+  agregarListeners() {
+    this.fav.current.addEventListener('click', () => this.toggleFavorito());
+  }
+
+  toggleFavorito() {
+    const body = { value: !this.esFavorito() };
+    API.put(`/${this.username}/fav/movie/${this.codigo}`, { ...body })
+      .then(() => this.alternarIconoFav())
+      .catch();
+  }
+
+  esFavorito() {
+    return this.state.favoritos.includes(this.codigo);
+  }
+
+  alternarIconoFav() {
+    this.fav.current.innerHTML = this.fav.current.innerHTML === 'star_border' ? 'star' : 'star_border';
+    this.traerFavs();
+  }
+
   codigoVideo() {
     return this.state.pelicula.link.match(/(?<==).*/g);
+  }
+
+  probarAlgo() {
+    console.log(this.state.favoritos);
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.pelicula.titulo}</h1>
-        <img src="img/star.svg" alt="icono de estrella" />
+        <div className="row row-show">
+          <h1>{this.state.pelicula.titulo}</h1>
+          <i ref={this.fav} className="material-icons">star_border</i>
+        </div>
         <iframe title={`trailer de ${this.state.pelicula.titulo}`} width="560" height="315" src={`https://www.youtube.com/embed/${this.codigoVideo()}`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        <button type="button" onClick={() => this.probarAlgo()}>Probar algo</button>
       </div>
     );
   }
